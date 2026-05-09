d6f8a5dc76fdaba8dc37835c922209f237de2fa6
angetresort <angetresort876@gmail.com>
2026-05-10 01:29:12 +0200

    rectifying css test 1 relative path in formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -112,7 +112,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Postuler | A&S-Web4All</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/Style.css">
 </head>
 <body>
     <header>
